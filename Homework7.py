aa400b1b1d135a342daf0056db698f59049e2ed1
Azerty <Azerty@DESKTOP-HHU9R9S>
2023-09-23 16:55:05 +0300

    Готово дз 7

--- Homework7.py
+++ Homework7.py
@@ -1,4 +1,5 @@
-﻿def check_rhythm(poem):
+﻿#Задача 34
+def check_rhythm(poem):
     phrases = poem.split()
     syllables = []
 
@@ -23,7 +24,22 @@ def count_vowels(word):
             count += 1
     return count
 
+def print_operation_table(operation, num_rows = 9, num_columns = 9):
+    for i in range(1, num_rows + 1):
+        row = []
+        for j in range(1, num_columns + 1):
+            row.append(operation(i, j))
+        print(*row)
+
+
+#Задача 34
 # Ввод стихотворения
-poem = input("Введите стихотворение: ")
-result = check_rhythm(poem)
-print(result)
+#poem = input("Введите стихотворение: ")
+#result = check_rhythm(poem)
+#print(result)
+
+# Функция умножения двух чисел
+multiply = lambda x, y: x * y
+
+# Вывод таблицы умножения
+print_operation_table(multiply)
